1df71c66e4648ecc413c285fcff622b8fd8471e6
Rustam <rustam905@gmail.com>
2021-03-21 19:23:12 -0700

    Header Options

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
 	return (
 		<div className='app'>
-			{/*Header*/}
-      <Header/>
+			<Header />
 			{/*App Body*/}
-			{/*SideBar*/}
-			{/*Feed*/}
-			{/*Widjets*/}
-			{/*App Body*/}
+			<div className='app__body'>
+				<Sidebar/>
+				{/*Feed*/}
+				{/*Widjets*/}
+				{/*App Body*/}
+			</div>
 		</div>
 	);
 }
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './css/Header.css'
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from './HeaderOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
     return (
@@ -12,12 +18,17 @@ function Header() {
 					/>
 					<div className='header__search'>
 						<SearchIcon />
-						<input type='text' placeholder="Search" />
+						<input type='text' placeholder='Search' />
 					</div>
 				</div>
 				<div className='header__right'>
-                    
-                </div>
+					<HeaderOption title='Home' Icon={HomeIcon} />
+					<HeaderOption title='My Network' Icon={SupervisorAccountIcon} />
+					<HeaderOption title='Jobs' Icon={BusinessCenterIcon} />
+					<HeaderOption title='Messaging' Icon={ChatIcon} />
+					<HeaderOption title='Notifications' Icon={NotificationsIcon} />
+                    <HeaderOption avatar = "https://qph.fs.quoracdn.net/main-qimg-8f1f832ddb81325276a55d371b10d913" title ="Me"/>
+				</div>
 			</div>
 		);
 }
