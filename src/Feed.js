29a3a8217be6eac1cd16ec5cd340f07152cb7d6e
Rustam <rustam905@gmail.com>
2021-03-22 19:46:44 -0700

    Building the sidebar

---
+++ src/Feed.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./css/Feed.css"
+import { Avatar } from "@material-ui/core";
+
+function Feed() {
+    return (
+			<div className='feed'>
+				<div className='feed__inputContainer'>
+					<Avatar className="avatar__input" />
+					<div className='feed__input'>
+						<form>
+							<input type='text' placeholder='Start a post' />
+							<button type='submit'>Send</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+}
+
+export default Feed
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
 import "./css/Sidebar.css";
+import { Avatar } from "@material-ui/core";
 
 function Sidebar() {
-    return (
-        <div className ="sidebar">
-            .sideva
-        </div>
-    )
+	const recentItem = (topic) => (
+		<div className='sidebar__recentItem'>
+			<span className='sidebar__hash'>#</span>
+			<p>{topic}</p>
+		</div>
+	);
+	return (
+		<div className='sidebar'>
+			<div className='sidebar__top'>
+				<img
+					src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+					alt=''
+				/>
+				<Avatar className='sidebar__avatar' />
+				<h2>Rustam Kolumbayev</h2>
+				<h4>rustam905@gmail.com</h4>
+			</div>
+			<div className='sidebar__stats'>
+				<div className='sidebar__stat'>
+					<p>Who viewed you</p>
+					<p className='sidebar__statNumber'>2,546</p>
+				</div>
+				<div className='sidebar__stat'>
+					<p>Views on post</p>
+					<p className='sidebar__statNumber'>2,448</p>
+				</div>
+			</div>
+			<div className='sidebar__bottom'>
+				<p>Recent</p>
+				{recentItem("react")}
+				{recentItem("reactstrap")}
+			</div>
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
