ec8cc66bb0898e702d79157e623cb054df3bb9b1
Rustam <rustam905@gmail.com>
2021-03-22 20:45:32 -0700

    Input Options

--- src/Feed.js
+++ src/Feed.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import "./css/Feed.css"
+import React from "react";
+import "./css/Feed.css";
 import { Avatar } from "@material-ui/core";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import EventIcon from "@material-ui/icons/Event";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 
 function Feed() {
-    return (
-			<div className='feed'>
-				<div className='feed__inputContainer'>
-					<Avatar className="avatar__input" />
+	return (
+		<div className='feed'>
+			<div className='feed__inputContainer'>
+				<div className='input'>
+					<Avatar className='avatar__input' />
 					<div className='feed__input'>
 						<form>
 							<input type='text' placeholder='Start a post' />
 							<button type='submit'>Send</button>
 						</form>
 					</div>
-                    <div className="feed__inputOptions">
-                        
-                    </div>
+				</div>
+				<div className='feed__inputOptions'>
+					<InputOption Icon={ImageIcon} title='Photo' color='#7085f9' />
+					<InputOption Icon={YouTubeIcon} title='Video' color='#32CD32' />
+					<InputOption Icon={EventIcon} title='Video' color='#FFA500' />
+					<InputOption
+						Icon={CalendarViewDayIcon}
+						title='Write article'
+						color='#FA8072'
+					/>
 				</div>
 			</div>
-		);
+
+            
+		</div>
+	);
 }
 
-export default Feed
+export default Feed;
